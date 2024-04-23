f4eab82a0034dc6aa2c972084b8823b6f0af21ee
alexgnunes <108927785+alexgnunes@users.noreply.github.com>
2024-04-23 06:28:27 -0300

    EX 1 - Crie um contador utiizando o useState

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 function Input({ children }){
-    return <input className="input" placeholder={ children }></input>
+    return <input className="input"value={children}></input>
 }
 
 export default Input;
